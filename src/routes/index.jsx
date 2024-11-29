7e006b93203a7be8acc5ed40884d0c180b6f7668
nqtien <nguyenquoctien2000@gmail.com>
2024-11-29 10:59:26 +0700

    Update: Tối ưu code , xoá tính năng thừa

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,7 @@
 import {createBrowserRouter} from 'react-router-dom'
 import ErrorPage from 'pages/errorPage'
 import Layout from 'layout/index'
-import CheckIdSr from 'pages/products/check-id-sr'
-import CheckIdEt from 'pages/products/check-id-et'
-import MetaEye from 'pages/products/meta-eye'
 // import IdPassPage from 'pages/IdPass'
-import CheckIdRt from 'pages/products/check-id-r301'
-import CheckIdHn from 'pages/products/check-id-hn202'
 import EkycPlatform from 'pages/solutions/EkycPlatform'
 import RarEkyc from 'pages/solutions/RarEkyc'
 
@@ -30,32 +25,6 @@ const Routers = createBrowserRouter([
           {
             path: 'ekyc-platform',
             element: <EkycPlatform />,
-          },
-        ],
-      },
-      {
-        path: 'products',
-        children: [
-          // Nhóm các route con vào trong 'products'
-          {
-            path: 'check-id-sr',
-            element: <CheckIdSr />,
-          },
-          {
-            path: 'check-id-et100',
-            element: <CheckIdEt />,
-          },
-          {
-            path: 'meta-eye',
-            element: <MetaEye />,
-          },
-          {
-            path: 'check-id-r301',
-            element: <CheckIdRt />,
-          },
-          {
-            path: 'check-id-hn212',
-            element: <CheckIdHn />,
           },
         ],
       },
